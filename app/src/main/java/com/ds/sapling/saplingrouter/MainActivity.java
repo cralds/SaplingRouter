a285d66c61b6687d0e5cf3c5839d97b46f012a2e
董帅 <dongshuai01@xhqb.com>
2019-07-23 17:37:16 +0800

    本地maven管理组件包

--- app/src/main/java/com/ds/sapling/saplingrouter/MainActivity.java
+++ app/src/main/java/com/ds/sapling/saplingrouter/MainActivity.java
@@ -1,7 +1,7 @@
 package com.ds.sapling.saplingrouter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.alibaba.android.arouter.launcher.ARouter;
